a5507aaf7cf80e339482d75493ce15d47912c192
Flor <friccomi@yahoo.com.ar>
2022-05-02 00:04:07 -0300

    first commit

---
+++ convert_train/s3_conn.py
@@ -0,0 +1,12 @@
+# s3_conn:
+AWS_REGION = "us-east-1"
+AWS_BUCKET_NAME = "friccomi"
+aws_access_key_id = "ASIA4H3EKUXNHFFS2TU2"
+aws_secret_access_key = "3G99adIk2o+KBRDhgpyFmNAlfBI3lcnvjEjUY8n6"
+aws_session_token = "FwoGZXIvYXdzEIH//////////wEaDBGVN6/mlf19fEgkcSK3AcIlidcUXimKrhvOdxcm0XNywM2IG05eIEQhun8tmmxj3E+x0Tnn6S0DSJHnIAN6GlsclplRo4uBEvw7+Tcw8jVslN90hvADO+PUsWGnmqgx30CJnoOPskuf29D13H5m/wWx/5UOrB6N1WIcaBKgF4dm/y/8mmkoLdSgA3SIW5Aq6hysHHqHAFVBl7CH33KQKQnfY5nFuh1wzOYO2Xd3MqPs+h/NJrXjoVCOLGhNeNTGjN9IeimbzSjvs7yTBjIte/+PZ08zQaXoWj9B1Ycdlqm4wTafuPfoZ5z64sSTyLH9z1Jd4FM3gqAdUDi7"
+AWS_ACCESS_KEY_ID = aws_access_key_id
+AWS_SECRET_ACCESS_KEY = aws_secret_access_key
+AWS_SESSION_TOKEN = aws_session_token
+AWS_EXECUTION_ROLE = "arn:aws:iam::841487132122:role/LabRole"
+origin = "s3"  # origin could be s3 or '' for local
+destiny = "s3"  # origin could be s3 or '' for local
---
+++ sql/create_env.sql
@@ -0,0 +1,5 @@
+-- Dumped from database version 9.6.3
+-- Dumped by pg_dump version 9.6.3
+
+CREATE SCHEMA "tp_final";
+ALTER SCHEMA "tp_final" OWNER TO airflow;
